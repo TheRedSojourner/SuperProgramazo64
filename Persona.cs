ddb1841136999e39f0db91dee1fe2a2c4289c656
icelandia <ice-return@hotmail.com>
2023-10-10 18:41:37 -0300

    Initial commit

---
+++ Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperProgramazo64
+{
+    internal class Persona
+    {
+        public bool esAburrido = false;
+        public int[] deudas;
+
+        public string nombre;
+
+        public bool GetEstado() { return esAburrido; }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿namespace SuperProgramazo64
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona jorge = new Persona();
+            Persona martin = new Persona();
+
+            List<Persona> personas = new List<Persona>();
+
+            personas.Add(jorge);
+            personas.Add(martin);
+            martin.nombre = "Martin";
+
+            personas.Remove(jorge);
+            AgregarPersona(personas);
+            RemovePersonaFromListByName(personas, "Gutz");
+
+        }
+
+        static void RemovePersonaFromListByName(List<Persona> personas, string nombre) 
+        {
+            
+            for (int i = 0; i < personas.Count; i++)
+            {
+                if (personas[i].nombre == nombre) { personas.RemoveAt(i); }   
+            }
+            
+        }
+
+        static void CrearPersona() 
+        {
+            Persona persona = new Persona();
+            persona.nombre = "Gutz";
+
+        }
+
+        static void CambiarEstadete(Persona persona) 
+        {
+            persona.esAburrido = true;
+        }
+        static void Sumar(out int x) 
+        {
+            x = 1;
+        }
+        static void SumarATodoElArray(ref int[] numeros) 
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i]++;
+
+            }
+            numeros = new int[5]; 
+        }
+
+        static void CambiarEstado(Persona persona) 
+        {
+            persona.esAburrido = !persona.esAburrido;
+        }
+
+    }
+}
